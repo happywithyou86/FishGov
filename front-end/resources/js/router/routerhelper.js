b7f656ef337eff9d7277abfb609c1fd77e84c9d2
canino_jories <canino_jories@hotmail.com>
2015-01-19 12:07:33 +0800

    restangular services

--- front-end/resources/js/router/routerhelper.js
+++ front-end/resources/js/router/routerhelper.js
@@ -2,27 +2,27 @@
     'use strict';
 
     angular
-        .module('blocks.router')
-        .provider('routehelperConfig', routehelperConfig)
-        .factory('routehelper', routehelper);
+      .module('blocks.router')
+      .provider('routehelperConfig', routehelperConfig)
+      .factory('routehelper', routehelper);
 
     routehelper.$inject = ['$location', '$rootScope', '$q', '$state', '$timeout', '$window',
       'logger', 'routehelperConfig'];
 
     function routehelperConfig() {
-        /* jshint validthis:true */
-        this.config = {
-            // These are the properties we need to set
-            // $routeProvider: undefined
-            // docTitle: ''
-            // resolveAlways: {ready: function(){ } }
-        };
+      /* jshint validthis:true */
+      this.config = {
+          // These are the properties we need to set
+          // $routeProvider: undefined
+          // docTitle: ''
+          // resolveAlways: {ready: function(){ } }
+      };
 
-        this.$get = function() {
-            return {
-                config: this.config
-            };
-        };
+      this.$get = function() {
+          return {
+              config: this.config
+          };
+      };
     }
 
     function routehelper( $location, $rootScope, $q, $state, $timeout, $window,
@@ -45,32 +45,32 @@
         return service;
 
         function configureRoutes(routes) {
-            routes.forEach(function(route) {
-                route.config.resolve =
-                    angular.extend( route.config.resolve || {}, routehelperConfig.config.resolveAlways );
-                $stateProvider.state( route.state, route.config );
-            });
-            $urlRouterProvider.otherwise('/');
+          routes.forEach(function(route) {
+              route.config.resolve =
+                  angular.extend( route.config.resolve || {}, routehelperConfig.config.resolveAlways );
+              $stateProvider.state( route.state, route.config );
+          });
+          $urlRouterProvider.otherwise('/');
         }
 
         function handleRoutingErrors() {
-            /***
-             ** Route cancellation
-             ***/
-            $rootScope.$on('$stateChangeError',
-                function(event, current, previous, rejection) {
-                    if (handlingRouteChangeError) {
-                        return;
-                    }
-                    routeCounts.errors++;
-                    handlingRouteChangeError = true;
-                    var destination = (current && (current.title || current.name || current.loadedTemplateUrl)) ||
-                        'unknown target';
-                    var msg = 'Error routing to ' + destination + '. ' + (rejection.msg || '');
-                    logger.warning(msg, [current]);
-                    $location.path('/');
-                }
-            );
+          /***
+           ** Route cancellation
+           ***/
+          $rootScope.$on('$stateChangeError',
+            function(event, current, previous, rejection) {
+              if (handlingRouteChangeError) {
+                  return;
+              }
+              routeCounts.errors++;
+              handlingRouteChangeError = true;
+              var destination = (current && (current.title || current.name || current.loadedTemplateUrl)) ||
+                  'unknown target';
+              var msg = 'Error routing to ' + destination + '. ' + (rejection.msg || '');
+              logger.warning(msg, [current]);
+              $location.path('/');
+            }
+          );
         }
 
         function init() {
@@ -79,14 +79,14 @@
         }
 
         function updateDocTitle() {
-            $rootScope.$on('$stateChangeSuccess',
-                function(event, toState, toParams, fromState, fromParams) {
-                  routeCounts.changes++;
-                  handlingRouteChangeError = false;
-                  var title = routehelperConfig.config.docTitle + ' ' + (toState.title || '');
-                  $rootScope.title = title;
-                }
-            );
+          $rootScope.$on('$stateChangeSuccess',
+            function(event, toState, toParams, fromState, fromParams) {
+              routeCounts.changes++;
+              handlingRouteChangeError = false;
+              var title = routehelperConfig.config.docTitle + ' ' + (toState.title || '');
+              $rootScope.title = title;
+            }
+          );
         }
     }
 })();
