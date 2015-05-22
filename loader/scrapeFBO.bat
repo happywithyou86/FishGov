3efaa1a8b1df84e254cd5f9eca54a5bfa7f9525e
Stephen McClanahan <steve.mcclanahan@gmail.com>
2015-05-22 18:46:46 -0400

    Batch file to autogenerate elasticsearch json

---
+++ loader/scrapeFBO.bat
@@ -0,0 +1,3 @@
+start call C:\Elastic\elasticsearch-1.5.2\bin\elasticsearch.bat
+python C:\Users\Stephen\Documents\GitHub\GovFish\loader\fedbizopps.py
+elasticdump --input http://127.0.0.1:9200/fishgov --output C:\Users\Stephen\Documents\GitHub\GovFish\loader\Sample.json
