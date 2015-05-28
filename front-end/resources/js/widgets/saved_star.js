1ffc8f934486a8abf1c3adc65ec8a56ef3672e5a
Jo-Ries Canino <canino_jories@hotmail.com>
2015-05-28 11:22:41 +0800

    adding watch scope in saved-star directive in search controller

--- front-end/resources/js/widgets/saved_star.js
+++ front-end/resources/js/widgets/saved_star.js
@@ -20,31 +20,10 @@
       return directive;
 
       function link(scope, element, attrs) {
-        scope.$watch('savedStar',function(newValue,oldValue) {
-          var saved_items     = JSON.parse(local_storage.getToken('saved_items')),
-              item_result     = JSON.parse(attrs.savedStar),
-              item_id         = item_result._id;
-
-          var position        = saved_items.indexOf(item_id);
-
-          if (position !== -1) {
-            element.removeClass('fa-star-o');
-            element.addClass('fa-star');
-          } else {
-            element.removeClass('fa-star');
-            element.addClass('fa-star-o');
-          }
-        });
-
-        var isAuthenticated = $auth.isAuthenticated();
-        if (!isAuthenticated) {
-          return;
-        }
-
-        var saved_items     = JSON.parse(local_storage.getToken('saved_items')),
-            item_result     = JSON.parse(attrs.savedStar),
-            item_id         = item_result._id;
-
+        var item_id,
+            item_result,
+            position,
+            saved_items;
         var title,
             keyword,
             solnbr,
@@ -54,33 +33,62 @@
             office,
             description;
 
-        /*test if we have item_id present*/
-        if (item_result.item_id !== undefined) {
-          item_id = item_result.item_id;
-          title       = item_result.title;
-          keyword     = item_result.keyword;
-          solnbr      = item_result.solnbr;
-          due_date    = item_result.close_date;
-          agency      = item_result.agency;
-          posted_date = item_result.posted_date;
-          office      = item_result.office;
-          description = item_result.description;
-        } else {
-          title       = item_result._source.title;
-          keyword     = $location.search().q;
-          solnbr      = item_result._source.solnbr;
-          due_date    = item_result._source.close_date;
-          agency      = item_result._source.agency;
-          posted_date = item_result._source.posted_date;
-          office      = item_result._source.office;
-          description = item_result.highlight.description[0] !== null ? item_result.highlight.description[0] : $rootScope.description;
+        var isAuthenticated = $auth.isAuthenticated();
+        if (!isAuthenticated) {
+          return;
         }
-        var position        = saved_items.indexOf(item_id);
 
-        if (position !== -1) {
-          element.removeClass('fa-star-o');
-          element.addClass('fa-star');
-        }
+        scope.$watch('savedStar',function(newValue,oldValue) {
+          saved_items     = JSON.parse(local_storage.getToken('saved_items'));
+          item_result     = JSON.parse(attrs.savedStar);
+          item_id         = item_result._id;
+
+          /*test if we have item_id present*/
+          if (item_result.item_id !== undefined) {
+            item_id     = item_result.item_id;
+            title       = item_result.title;
+            keyword     = item_result.keyword;
+            solnbr      = item_result.solnbr;
+            due_date    = item_result.close_date;
+            agency      = item_result.agency;
+            posted_date = item_result.posted_date;
+            office      = item_result.office;
+            description = item_result.description;
+          } else {
+            title       = item_result._source.title;
+            keyword     = $location.search().q;
+            solnbr      = item_result._source.solnbr;
+            due_date    = item_result._source.close_date;
+            agency      = item_result._source.agency;
+            posted_date = item_result._source.posted_date;
+            office      = item_result._source.office;
+            description = item_result.highlight.description[0] !== null ? item_result.highlight.description[0] : $rootScope.description;
+          }
+
+          position  = saved_items.indexOf(item_id);
+          if (position !== -1) {
+            element.removeClass('fa-star-o');
+            element.addClass('fa-star');
+          } else {
+            element.removeClass('fa-star');
+            element.addClass('fa-star-o');
+          }
+        });
+
+
+        // var saved_items     = JSON.parse(local_storage.getToken('saved_items')),
+        //     item_result     = JSON.parse(attrs.savedStar);
+        //     item_id         = item_result._id;
+        //
+        // var position        = saved_items.indexOf(item_id);
+
+
+        // var position        = saved_items.indexOf(item_id);
+        //
+        // if (position !== -1) {
+        //   element.removeClass('fa-star-o');
+        //   element.addClass('fa-star');
+        // }
 
         element.on('click', function() {
           var hasClass = element.hasClass('fa-star-o');
@@ -91,6 +99,7 @@
             /*set the localStorage again for the saved_items*/
             saved_items = JSON.parse(local_storage.getToken('saved_items'));
             local_storage.removeToken('saved_items');
+            console.log(item_id);
             saved_items.push(item_id);
             saved_items = local_storage.setToken('saved_items', JSON.stringify(saved_items));
 
