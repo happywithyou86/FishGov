31ef8312998e96691cc63f9b52c55c04d948281f
Stephen McClanahan <steve.mcclanahan@gmail.com>
2015-05-30 00:55:37 -0400

    Deeper scrapping, error handling

--- loader/fedbizopps.py
+++ loader/fedbizopps.py
@@ -50,7 +50,7 @@ def parse_index(html):
     def parse_link(url, i=0):
         
         #print("%d. URL: " % i + url)
-		
+        time.sleep(5)
         data = {}
         html = requests.get(url).text
         
@@ -94,6 +94,23 @@ def parse_index(html):
         except:
             print(html)
             data["description"] = ""
+
+        try:
+            additional_info = bs_content.find("div", {"id": "dnf_class_values_procurement_notice__additional_info_link__widget"})
+            data["additional_info_link"] = additional_info.find("a")["href"].strip()
+            data["additional_info_description"] = additional_info.find("a").text.strip()
+        except:
+            data["additional_info_link"] = ""
+            data["additional_info_description"] = ""
+            
+        if re.search('single source', data["description"], re.IGNORECASE):
+            data["is_sole_source"] = True
+        elif re.search('sole source', data["description"], re.IGNORECASE):
+            data["is_sole_source"] = True
+        elif re.search('other than full and open competition', data["description"], re.IGNORECASE):
+            data["is_sole_source"] = True
+        else:
+            data["is_sole_source"] = False
 			
 		# These fields can be pulled directly from the respective class
         extract = {
@@ -109,7 +126,15 @@ def parse_index(html):
             
             "contracting_office_address": "dnf_class_values_procurement_notice__office_address__widget",
             "primary_point_of_contact": "dnf_class_values_procurement_notice__primary_poc__widget",
+            "archive_date":"dnf_class_values_procurement_notice__archive_date__widget",
+            "archive_type":"dnf_class_values_procurement_notice__archive_type__widget",
+            "place_of_performance":"dnf_class_values_procurement_notice__place_of_performance__widget",
+            "secondary_point_of_contact":"dnf_class_values_procurement_notice__secondary_poc__widget",
         }
+        
+        # add additional info link from here
+        # dnf_class_values_procurement_notice__additional_info_link__widget
+        # https://www.fbo.gov/index?s=opportunity&mode=form&id=56f7098887f8ba9d8cc755e74e15252d&tab=core&_cview=0
         
         for key, id_ in extract.items():
             try:
@@ -130,14 +155,15 @@ def parse_index(html):
         # If there was no original posted date as in the case of an original solicitation use last posted date
         if data["posted_date"] == "":
             data["posted_date"] = data["last_posted_date"]
-            
+        
+  
+        # Get the logo
         try:
             desc = bs_content.find("div", {"class": "agency-logo"})
-            print(str(desc))
-            data["logo_url"]= urljoin("https://www.fbo.gov/index", desc.find("img")["src"].strip())
-            print(data["logo_URL"])
+            data["logo_url"] = urljoin("https://www.fbo.gov/index", desc.find("img")["src"])
         except:
             print("Error getting logo URL")
+            data["logo_url"] = ""
         
         if data["notice_type"] == "Award":
             return ""
@@ -209,42 +235,42 @@ def parse_index(html):
 
 def main():
 
-    host = '192.96.159.94'
+    host = '192.96.159.93'
     username = 'canino_jories'
     password = 'joriescanino'
-    port = 22
+    port = 22500
         
-    # Setup FTP to push bulk file to server
-    transport = paramiko.Transport((host, port))
-    transport.connect(username=username, password=password)
-    sftp = paramiko.SFTPClient.from_transport(transport)
 
-    # Setup SSH to command curl for PUT of bulk file locally on remote host
-    i = 1
-    while True:
-        print("Trying to connect to %s (%i/30)" % (host, i))
-
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(host, username=username, password=password)
-            print("Connected to %s" % host)
-            break
-        except paramiko.AuthenticationException:
-            print("Authentication failed when connecting to %s" % host)
-            sys.exit(1)
-        except:
-            print("Could not SSH to %s, waiting for it to start" % host)
-            i += 1
-            time.sleep(2)
-
-        # If we could not connect within time limit
-        if i == 30:
-            print("Could not connect to %s. Giving up" % host)
-            sys.exit(1)
         
     # Loop through the different search result pages
-    for num in range(10,12):
+    for num in range(3,309):
+
+        # Setup FTP to push bulk file to server
+        transport = paramiko.Transport((host, port))
+        transport.connect(username=username, password=password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        # Setup SSH to command curl for PUT of bulk file locally on remote host
+        i = 1
+        while True:
+            print("Trying to connect to %s (%i/30)" % (host, i))
+
+            try:
+                ssh = transport.open_channel(kind='session')
+            
+                print("Connected to %s" % host)
+                break
+            
+            except:
+                print("Could not SSH to %s, waiting for it to start" % host)
+                i += 1
+                time.sleep(2)
+
+            # If we could not connect within time limit
+            if i == 30:
+                print("Could not connect to %s. Giving up" % host)
+                sys.exit(1)
+
         html = get_index(num)
         print ('Downloading solicitation data from page ' + str(num))
         parse_index(html)
@@ -255,20 +281,29 @@ def main():
         sftp.put(localpath, filepath)
     
         # Command the POST of the bulk file into Elasticsearch
-        stdin, stdout, stderr = ssh.exec_command("curl -s -XPOST localhost:9200/_bulk --data-binary @Projects/github/preload_fedbizopps.json; echo")
-        print(stdout.channel.recv_exit_status())
-        # Wait for the command to terminate
-        while not stdout.channel.exit_status_ready():
-            # Only print data if there is data to read in the channel
-            if stdout.channel.recv_ready():
-               print(stdout.readlines())
+        ssh.exec_command("curl -s -XPOST localhost:9200/_bulk --data-binary @Projects/github/preload_fedbizopps.json; echo")
+        
+        nbytes = 4096
+        stdout_data = []
+        stderr_data = []
+        while True:
+            if ssh.recv_ready():
+                stdout_data.append(ssh.recv(nbytes))
+            if ssh.recv_stderr_ready():
+                stderr_data.append(ssh.recv_stderr(nbytes))
+            if ssh.exit_status_ready():
+                break
+        
+        # Close up
+        sftp.close()
+        transport.close()
+        ssh.close()
         
         os.remove('C:\\Users\\Stephen\\preload_fedbizopps.json')
+        time.sleep(720)
         
-    # Close up
-    sftp.close()
-    transport.close()
-    ssh.close()
+        
+    
 
 
 if __name__ == '__main__':
