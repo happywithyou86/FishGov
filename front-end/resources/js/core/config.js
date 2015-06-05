0e3a471f946148547ad28945143147759c7b5baa
Jo-Ries Canino <canino_jories@hotmail.com>
2015-06-05 10:25:27 +0800

    adding changes in the layout view for fbo offsite

--- front-end/resources/js/core/config.js
+++ front-end/resources/js/core/config.js
@@ -28,6 +28,7 @@
       var ua = navigator.userAgent,
           tem,
           M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
       if(/trident/i.test(M[1])){
         tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
         return {name:'IE ',version:(tem[1]||'')};
@@ -38,6 +39,7 @@
       }
       M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
       if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+
       if (M[0] === 'Chrome') {
         $rootScope.validateBrowser = true;
         if ((parseInt(M[1]) - 5) < 38) {
