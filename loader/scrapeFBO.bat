f424e07c2326d4f2a98b409aa820855a95123687
Stephen McClanahan <steve.mcclanahan@gmail.com>
2015-06-07 06:49:27 -0400

    Delete scrapeFBO.bat

--- loader/scrapeFBO.bat
+++
@@ -1,3 +0,0 @@
-start call C:\Elastic\elasticsearch-1.5.2\bin\elasticsearch.bat
-python C:\Users\Stephen\Documents\GitHub\GovFish\loader\fedbizopps.py
-elasticdump --input http://127.0.0.1:9200/fishgov --output C:\Users\Stephen\Documents\GitHub\GovFish\loader\Sample.json
